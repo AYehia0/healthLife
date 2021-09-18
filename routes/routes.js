890ce38a027c86dc91d229d2fc6300941f8f1f24
AYehia0 <ahmedyehia611@gmail.com>
2021-09-18 22:39:38 +0200

    added curl examples

--- routes/routes.js
+++ routes/routes.js
@@ -5,12 +5,16 @@ const controller = require('../controller/controller')
 router.get('/', controller.main)
 
 // add recipe
+// curl -d '{"name": "pie pie", "summary": "The sweetest apple pie ever made", "category": "junk", "facts": { "fats":20, "proteins":10, "carbs":30 }}' -H "Content-Type: application/json" -X POST http://localhost:8080/recipe
 router.post('/recipe', controller.addRecipe)
 
 // get recipes
+// search by name + range ,,, 0 range returns all
+// curl -X GET "localhost:8080/recipe?name=pie&range=0" 
 router.get('/recipe', controller.getRecipes)
 
 // edit recipe
+// curl -d '{"name": "not a pie", "summary": "The sweetest apple pie ever made", "category": "junk", "facts": { "fats":20, "proteins":10, "carbs":30 }}' -H "Content-Type: application/json" -X PUT  http://localhost:8080/recipe/61461803d182194ca7c4ffab 
 router.put('/recipe/:id', controller.editRecipe)
 
 
