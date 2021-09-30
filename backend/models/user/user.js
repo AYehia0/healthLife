2473e0ecfd8163fe50a5ed224188661ad5091ff8
AYehia0 <ahmedyehia611@gmail.com>
2021-09-30 17:46:13 +0200

    added needs, moved the meals to user root, and calculated needs on save

--- backend/models/user/user.js
+++ backend/models/user/user.js
@@ -103,28 +103,71 @@ const userSchema = new Schema({
                 type: Number,
                 default: 0
             },
+            needs: {
+                protein: {
+                    type: Number,
+                    default: 0
+                },
+                carb: {
+                    type: Number,
+                    default: 0
+                },
+                fat: {
+                    type: Number,
+                    default: 0
+                }
+            },
             favourites: [
                 {
-                    type: {
-                        type: String,
-                        enum: ['breakfast', 'dinner', 'launch', 'snack', 'workout'],
-                        required: true
-                    },
-                    mealId: mealId
+                    // set
+                    types:[
+                        {
+                            type: String,
+                            enum: ['breakfast', 'dinner', 'launch', 'snack', 'workout'],
+                            required: true
+                        }
+                   ],
+
+                    mealId: mealId,
+            
                 }
             ],
-            meals: [{
-                    date: {
-                        type: Date,
-                        default: Date.now
-                    },
-                    //workout: []
-                }]
         },
         required: function () {
             return this.role === 'user'
         }
    },
+
+    food: {
+        type:{
+            meals: [{
+            date: {
+                type: Date,
+                default: Date.now
+            },
+            mealId: mealId,
+            categories: [{   
+                type: {
+                    type: String,
+                    enum: ['breakfast', 'dinner', 'launch', 'snack', 'workout'],
+                    required: true
+                },
+                count: {
+                    type: Number,
+                    default: 1
+                },
+                category: {
+                    type: String,
+                    required: true
+                }
+            }]
+        }]},
+        required: function() {
+            return this.role === 'user'
+        }
+    },
+
+
 }, 
 {
     // logging modification
@@ -141,7 +184,7 @@ const userSchema = new Schema({
 }})
 
 // hashing the password on save 
-userSchema.pre('save', function(){
+userSchema.pre('save', function(next){
     // getting the user
     const user = this
 
@@ -149,8 +192,92 @@ userSchema.pre('save', function(){
     if(user.isModified('password')){
         user.password = bcrybt.hashSync(user.password, 12) 
     }
+    next()
+})
+/*
+
+For men: BMR = 10 x weight (kg) + 6.25 x height (cm) – 5 x age (years) + 5
+For women: BMR = 10 x weight (kg) + 6.25 x height (cm) – 5 x age (years) – 161
+
+1.55 = Light: Typing, teaching, lab/shop work, some walking throughout the day
+1.65 = Moderate: Walking, jogging, gardening type job with activities such as cycling, tennis, dancing, skiing or weight training 1-2 hours per day
+1.80 = Heavy: Heavy manual labor such as digging, tree felling, climbing, with activities such as football, soccer or body building 2-4 hours per day
+2.00 = Very Heavy: A combination of moderate and heavy activity 8 or more hours per day, plus 2-4 hours of intense training per day
+
+Your Total Daily Energy Expenditure (TDEE) = BMR x Activity Level Multiplier
+
+*/
+
+// calculating the cals needed
+const calorieNeeds = (age, weight, height, gender, activityLvl) => {
+
+    //['low', 'mod', 'high', 'ext'],
+    activityLvls = {
+        low: 1.55,
+        mod: 1.65,
+        high: 1.80,
+        ext: 2.00
+    }
+
+    // calculating BMR
+    const bmr = {
+        male: 10 * weight + 6.25 * height - 5 * age + 5,
+        female: 10 * weight + 6.25 * height - 5 * age - 161,
+    }
+
+    return bmr[gender] * activityLvls[activityLvl]
+
+}
+
+// ref : https://www.k-state.edu/paccats/Contents/Nutrition/PDF/Needs.pdf
+const proteinFatCarbNeeds = (cals, weight) => {
+
+    // Protein : 1g for kg
+    const percent = 1
+    const protein = percent * weight
+    const fat = (cals * 0.3) / 9 
+    const carb = (cals - fat * 9 - protein * 4) / 4
+
+    // Fat intake should equal 30% of your total days calories. 
+
+    return [Math.round(protein), Math.round(fat), Math.round(carb)]
+
+}
+
+userSchema.pre('save', async function(next){
+
+    // getting the food item
+    const user = this
+
+    // checking if the registerd user is admin
+    if (user.role == "admin"){
+
+        next()
+
+    }
+    const personal = user.personal
+
+    // calculating the calories
+    const cals = Math.round(calorieNeeds(personal.age, personal.weight, personal.height, personal.gender, personal.activity))
+
+    // calculating protein, carb, fat
+    const needs = proteinFatCarbNeeds(cals, personal.weight)
+    
+    //[protein, fat, carb] = needs
+    const protein = needs[0]
+    const fat = needs[1]
+    const carb = needs[2]
+
+    // modifiy
+    user.personal.startCals = cals
+    user.personal.needs.protein = protein
+    user.personal.needs.fat = fat 
+    user.personal.needs.carb = carb
+
+    next()
 
 })
+
 
 // login user 
 userSchema.statics.login = async (email, password, role) => {
